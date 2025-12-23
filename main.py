7664e51e78121075564d62003dccb1bc75cfa1a8
Ash-r98 <duncanjgmcintosh@gmail.com>
2025-12-23 22:21:33 +0000

    Added role placeholders and playercount/4 werewolves

--- main.py
+++ main.py
@@ -18,14 +18,18 @@ def intinputvalidate(prompt, lower, upper):
 def clearscreen():
     os.system('cls' if os.name == 'nt' else 'clear') # Wipe terminal
 
-players = []
+# Roles: 0 - Villager, 1 - Werewolf, 2 - Naughty Girl, 3 - Drunk, 4 - Hunter, 5 - Jester, 6 - Sheriff
+rolenames = ["Villager", "Werewolf", "Naughty Girl", "Drunk", "Hunter", "Jester", "Sheriff"]
+
+playerlist = []
+roleslist = []
 
 while True:
     newplayername = input("Input player name: (Input '0' once all players are in or '-1' to reset all names)\n")
     if newplayername == '0':
-        if len(players) >= 3:
-            print(f"All {len(players)} players:")
-            for name in players:
+        if len(playerlist) >= 3:
+            print(f"All {len(playerlist)} players:")
+            for name in playerlist:
                 print(name)
             allplayernamesconfirm = intinputvalidate("Is this all players? (1=Yes, 0=No, add more)\n", 0, 1)
             if allplayernamesconfirm:
@@ -33,10 +37,29 @@ while True:
         else:
             print("You must have at least 3 players to play")
     elif newplayername == '-1':
-        players = []
+        playerlist = []
+        roleslist = []
         print("All names reset, please input all player names again")
     else:
         print(f"Name: {newplayername}")
         playernameconfirm = intinputvalidate("Add name to player list? (1=Yes, 0=No)\n", 0, 1)
         if playernameconfirm:
-            players.append(newplayername)
+            playerlist.append(newplayername)
+            roleslist.append(0)
+
+playernum = len(playerlist)
+werewolfnum = playernum // 4
+if werewolfnum <= 0:
+    werewolfnum = 1
+
+werewolfidlist = []
+for i in range(werewolfnum):
+    while True:
+        newwerewolfid = randint(0, playernum-1)
+        if newwerewolfid not in werewolfidlist:
+            werewolfidlist.append(newwerewolfid)
+            break
+
+for i in range(len(roleslist)):
+    if i in werewolfidlist:
+        roleslist[i] = 1
